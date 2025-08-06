bf5e697665959c015293b27f6736550e952c6cdb
James <nathanjamessnr@gmail.com>
2025-08-06 14:47:23 +0330

    Fix case-sensitive imports for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout.jsx";
-import Home from "./pages/home";
-import Destination from "./pages/destination";
-import Crew from "./pages/crew";
-import Technology from "./pages/technology";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 
 function App() {
   return (
